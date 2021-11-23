5231977203720f475a0692a42c2eb624ec2fd81b
Alex Astrum <alexastrum@gmail.com>
2021-11-23 17:07:55 -0500

    Add Quasar

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-view />
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+// Initialize Firebase
+export default defineComponent({
+  name: 'App',
+  setup() {
+  }
+})
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <q-btn @click="signIn">Sign in with Google</q-btn>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+
+
+export default defineComponent({
+  name: 'PageIndex',
+  setup() {
+    async function signIn() {
+      console.log('Sign in!');
+
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth();
+      await signInWithPopup(auth, provider);
+
+    }
+    return { signIn };
+  }
+});
+</script>
